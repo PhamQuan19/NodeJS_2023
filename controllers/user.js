d8a782bcc2230a2d152a8ad932a657563c44b920
Phạm Quân <phamquan29hy@gmail.com>
2024-01-11 16:00:25 +0700

    Trả về dữ liệu dạng JSON kèm HTTP Status Code

--- controllers/user.js
+++ controllers/user.js
@@ -2,14 +2,21 @@ import { body, validationResult } from "express-validator";
 const login = async (req, res) => {
   const error = validationResult(req);
   if (!error.isEmpty()) {
-    return res.status(400).json({ error: error.array() });
+    return res.status(400).json({ 
+      error: error.array()
+     });
   }
   const { email, password } = req.body;
-  res.send("Post login users");
+  res.status(200).json({
+    message:'login user successfully',
+    // data: "detail user ..."
+  });
 };
 
 const register =async(req, res) =>{
-    res.send('Post register users')
+    res.status(201).json({
+      message: 'register user successfully'
+    });
 }
 
 const getDetailUser=async(req,res) =>{
